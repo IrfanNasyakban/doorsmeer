025ff31e99b5438e770cbf35adeb49a5ffaab73f
Irvan Na'syakban <irfanna.syakban098@gmail.com>
2023-06-25 17:22:32 +0700

    Update

---
+++ admin.php
@@ -0,0 +1,131 @@
+<?php
+session_start();
+if( empty( $_SESSION['id_user'] ) ){
+	//session_destroy();
+	$_SESSION['err'] = '<strong>ERROR!</strong> Anda harus login terlebih dahulu.';
+	header('Location: ./index.php');
+	die();
+} else {
+	include "koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Aplikasi Jasa Cuci</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/jquery-ui.min.css" rel="stylesheet">
+
+    <script src="js/jquery.min.js"></script>
+
+
+	<style type="text/css">
+	body {
+	  min-height: 200px;
+	  padding-top: 70px;
+	  background-image: linear-gradient(
+      115deg,
+      rgba(58, 58, 158, 0.8),
+      rgba(136, 136, 206, 0.7)
+    ),
+    url(image/background.jpg);
+  	background-size: cover;
+	}
+	.jumbotron {
+		background:rgba(1,1,1,0.5);
+	}
+	.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    padding: 20px;
+    margin-top: 20px;
+    flex-basis: 30%;
+    text-align: center;
+	}
+	</style>
+
+  </head>
+
+  <body>
+
+    <?php include "menu.php"; ?>
+
+    <div class="container">
+
+	<?php
+	if( isset($_REQUEST['hlm'] )){
+
+		$hlm = $_REQUEST['hlm'];
+
+		switch( $hlm ){
+			case 'transaksi':
+				include "transaksi.php";
+				break;
+			case 'user':
+				include "user.php";
+				break;
+			case 'cetak':
+				include "cetak_nota.php";
+				break;
+		}
+	} else {
+	?>
+      <!-- Main component for a primary marketing message or call to action -->
+		<div class="jumbotron">
+			<div class="text-center">
+				<img src="image/logo.png" class="rounded" alt="logo" style="max-width: 360px;">
+			</div>
+			<h2>Selamat Datang di Aplikasi Kasir Jasa Cuci</h2>
+
+        <p style="color: #bfe1e7;">Halo <strong><?php echo $_SESSION['nama']; ?></strong>, Anda login sebagai
+			<strong>
+			<?php
+				if($_SESSION['level'] == 1){
+					echo 'Admin.';
+				} else {
+						echo 'Petugas Kasir.';
+				}
+			?>
+			</strong>
+		</p>
+      </div>
+	  <div class="jumbotron" style="color: #bfe1e7;">
+	  <div class="row text-center">
+  		<div class="col-md-4">
+		  <p>Irvan Nasyakban</p>
+		  <img src="image/irvan.png" style="width: 150px;">
+		</div>
+  		<div class="col-md-4">
+		  <p>Widia Hamsi</p>
+          <img src="image/widia.png" style="width: 150px;">
+		</div>
+  		<div class="col-md-4">
+		  <p>Muhammad Ariansyah</p>
+          <img src="image/ari.png" style="width: 150px;">
+		</div>
+	  </div>
+	  </div>
+	<?php
+	}
+	?>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript, Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	
+  </body>
+
+</html>
+<?php
+}
+?>
---
+++ database/ajc_dua.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2022 at 06:46 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ajc_dua`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biaya`
+--
+
+CREATE TABLE `biaya` (
+  `id_biaya` int(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `biaya` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `biaya`
+--
+
+INSERT INTO `biaya` (`id_biaya`, `jenis`, `biaya`) VALUES
+(2, 'Sepeda Motor', 10000),
+(5, 'Mobil', 25000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(10) NOT NULL,
+  `no_nota` varchar(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `bayar` int(10) NOT NULL,
+  `kembali` int(10) NOT NULL,
+  `total` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_user` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `no_nota`, `jenis`, `nama`, `bayar`, `kembali`, `total`, `tanggal`, `id_user`) VALUES
+(63, 'C001', 'Sepeda Motor', 'Azis', 20000, 10000, 10000, '2022-12-10', 11),
+(64, 'C002', 'Mobil', 'Agung', 50000, 25000, 25000, '2022-12-10', 11),
+(65, 'C003', '10000', 'Ari', 25000, 15000, 10000, '2022-12-10', 11),
+(66, 'C004', 'Mobil', 'John Doe', 30000, 5000, 25000, '2022-12-10', 11),
+(67, 'C005', 'Mobil', 'Messi', 20000, 10000, 10000, '2022-12-10', 11),
+(74, 'C006', 'Sepeda Motor', 'dfrf', 50000, 25000, 25000, '2022-12-11', 11),
+(75, 'C007', 'Sepeda Motor', 'sdsw', 50000, 20000, 30000, '2022-12-11', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `level` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `hp`, `level`) VALUES
+(1, 'masrud', '7d05dc02abe9cda729d0c798c886db47', 'Rudi', 'Sawahan, Nganjuk, Jawa Timur', '085232900000', 1),
+(11, 'irfan', '24b90bc48a67ac676228385a7c71a119', 'Irvan Na', 'Langsa', '0836847836', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biaya`
+--
+ALTER TABLE `biaya`
+  ADD PRIMARY KEY (`id_biaya`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `biaya`
+--
+ALTER TABLE `biaya`
+  MODIFY `id_biaya` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
